4452a460b8e8ba4a0b358382dde5543ce25cd2d6
joaovicttors <joaovicttorrs@gmail.com>
2022-06-02 11:24:26 -0300

    add room on data module

--- features/character/data/src/main/java/com/joaovicttors/data/models/CharacterEntity.kt
+++ features/character/data/src/main/java/com/joaovicttors/data/models/CharacterEntity.kt
@@ -1,11 +1,24 @@
 package com.joaovicttors.data.models
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import com.joaovicttors.bases.data.BaseModel
 
+@Entity(tableName = "character")
 data class CharacterEntity(
-    val id: Int?,
-    val name: String?,
-    val thumbnail: String?,
-    val description: String?,
-    val resourceUri: String?,
-) : BaseModel()
+    @ColumnInfo(name = ID) @PrimaryKey val id: Int?,
+    @ColumnInfo(name = NAME) val name: String?,
+    @ColumnInfo(name = THUMBNAIL) val thumbnail: String?,
+    @ColumnInfo(name = DESCRIPTION) val description: String?,
+    @ColumnInfo(name = RESOURCE_URI) val resourceUri: String?,
+) : BaseModel() {
+
+    private companion object {
+        private const val ID: String = "id"
+        private const val NAME: String = "name"
+        private const val THUMBNAIL: String = "thumbnail"
+        private const val DESCRIPTION: String = "description"
+        private const val RESOURCE_URI: String = "resourceURI"
+    }
+}
